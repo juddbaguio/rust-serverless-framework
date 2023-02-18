91c21d2b53eb14bbd313708ee92faed46767bed9
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2023-02-18 20:08:32 +0800

    added oneshot

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ use std::{env::var, sync::Arc};
 
 use axum::{extract::State, response::Redirect, routing::get, Router};
 
-#[tokio::main]
-async fn main() {
+fn app() -> Router {
     let nickname = Arc::new(match var("NICKNAME") {
         Ok(val) => val,
         _ => "".to_string(),
     });
 
-    let app = Router::new()
+    Router::new()
         .route("/", get(|| async { "Hello, world! I am Judd" }))
         .route(
             "/github",
@@ -25,10 +24,32 @@ async fn main() {
                 return Redirect::permanent(&format!("https://www.twitter.com/{:?}", nickname));
             }),
         )
-        .with_state(nickname);
+        .with_state(nickname)
+}
 
+#[tokio::main]
+async fn main() {
+    let route_app = app();
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(route_app.into_make_service())
         .await
         .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        extract::connect_info::MockConnectInfo,
+        http::{self, Request, StatusCode},
+    };
+    use tower::ServiceExt; // for `oneshot` and `ready`
+
+    #[tokio::test]
+    async fn test_redirect() {
+        let app = app();
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        app.oneshot(req).await.unwrap();
+    }
 }
